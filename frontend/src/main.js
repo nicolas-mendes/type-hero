97b141a3b1ef3c5c69bc686c5f620d10470d9750
Nicolas Mendes Fernandes <nicolasmendes512@gmail.com>
2025-11-26 17:27:08 -0300

    Arquivo principal do frontend que instancia o Game e possui as configurações principais

---
+++ frontend/src/main.js
@@ -0,0 +1,27 @@
+import { Preload } from './scenes/Preload.js';
+import { Login } from './scenes/Login.js';
+
+const config = {
+    type: Phaser.AUTO,
+    title: 'Type Hero',
+    description: 'Um jogo de digitação',
+    parent: 'game-container',
+    width: 1280,
+    height: 720,
+    backgroundColor: '#000000',
+    pixelArt: false,
+    dom: {
+        createContainer: true
+    },
+    scene: [
+        Preload,
+        Login
+    ],
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
+}
+
+new Phaser.Game(config);
+            
